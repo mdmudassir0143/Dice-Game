c6cfb687779d47c1343228bed2f858b7b8f44fdf
Mohammad Mudassir <67314024+mdmudassir0143@users.noreply.github.com>
2022-06-28 18:55:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+var randomNumber1 = Math.floor(Math.random() * 6)+1;
+
+var randomdice ="dice" + randomNumber1 + ".png";
+
+var source = "./images/" + randomdice;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", source);
+
+
+
+var randomNumber2 = Math.floor(Math.random() * 6)+1;
+
+var randomdice2 ="./images/dice" + randomNumber2 + ".png";
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src", randomdice2);
+
+
+if (randomNumber1>randomNumber2) {
+    document.querySelector("h1").innerHTML="Player 1 win"
+}
+else if (randomNumber1<randomNumber2){
+    document.querySelector("h1").innerHTML="Player 2 win"
+}
+else{
+    document.querySelector("h1").innerHTML="Draw"
+}
